70e3b44ce3114b144d7be76bdca1f48dfc7ab771
Pathurs <pathurs123@gmail.com>
2018-11-10 21:34:51 +1100

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+declare class SemanticReleaseError extends Error {
+    public name: string;
+    public message: string;
+    public code: string;
+    public details: string;
+    constructor(message: string, code: string, details: string);
+}
+
+export = SemanticReleaseError;
