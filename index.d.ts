4b52a4c7b178caff8af5fef0b5ec401b09443bb0
Pathurs <pathurs123@gmail.com>
2018-11-13 10:33:32 +1100

    fix: make code and details optional
    message default to `""` when `new Error()` is provided no message

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
 declare class SemanticReleaseError extends Error {
     public name: string;
     public message: string;
-    public code: string;
-    public details: string;
+    public code?: string;
+    public details?: string;
     constructor(message?: string, code?: string, details?: string);
 }
 
