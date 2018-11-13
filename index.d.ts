037a1b11555ac9386c0a158f5a457ccab2316fd6
Pathurs <pathurs123@gmail.com>
2018-11-13 20:39:55 +1100

    chore: remove redundant declarations

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,4 @@
 declare class SemanticReleaseError extends Error {
-    public name: string;
-    public message: string;
     public code?: string;
     public details?: string;
     constructor(message?: string, code?: string, details?: string);
